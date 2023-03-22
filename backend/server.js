2cec40c53d3c8bd6e7d43c5f3f6f22474c1041f5
minhthuong031103 <nominh67@gmail.com>
2023-03-22 16:19:06 +0700

    Add server nodejs express
    - Run npm init in backend folder
    - set type module for json
    - npm install express
    - create server.js

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import data from "./data.js";
+const app = express();
+//test
+//sd
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+const port = process.env.port || 5000;
+app.listen(port, () => {
+  console.log(`server at https://localhost:${port}`);
+});
